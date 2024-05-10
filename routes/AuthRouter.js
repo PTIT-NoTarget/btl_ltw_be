f640da33d7894f1315d6f79820f0fd65cafe4e4b
PTIT-NoTarget <HanND.B21CN324@stu.ptit.edu.vn>
2024-05-10 21:01:58 +0700

    init

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -4,6 +4,16 @@ const jwt = require("jsonwebtoken");
 const User = require("../model/UserModel");
 const router = express.Router();
 const upload = require("../utils/Upload");
+const validateToken = require("../utils/ValidateToken");
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (password, hash) => {
+  return await bcrypt.compare(password, hash);
+}
 
 router.post("/login", async (request, response) => {
   const { username, password } = request.body;
@@ -18,19 +28,6 @@ router.post("/login", async (request, response) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
   response.header("auth-token", token).json({ token });
 });
-
-router.post("/logout", async (request, response) => {
-  
-});
-
-const hashPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
-};
-
-const comparePassword = async (password, hash) => {
-  return await bcrypt.compare(password, hash);
-}
 
 router.post("/register", upload("avatar"), async (request, response) => {
   const user = new User({
@@ -54,6 +51,24 @@ router.post("/register", upload("avatar"), async (request, response) => {
     .catch((error) => {
       response.status(400).json({ message: error.message });
     });
+});
+
+router.post("/reset-password", async (request, response) => {
+  validateToken(request, response, async (userId) => {
+    const user = await User.findOne({ _id: userId });
+    const validPassword = await comparePassword(request.body.oldPassword, user.password);
+    if (!validPassword) {
+      return response.status(400).json({ message: "Old password is incorrect" });
+    }
+    user.password = await hashPassword(request.body.newPassword);
+    user.save()
+      .then((data) => {
+        response.json(data);
+      })
+      .catch((error) => {
+        response.status(400).json({ message: error.message });
+      });
+  });
 });
 
 module.exports = router;
